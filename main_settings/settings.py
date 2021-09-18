e1b9142c3a2506a2ab03dd2e90a4ff6b60ad6aad
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2021-09-18 13:55:42 +0800

    Update settings.py

--- main_settings/settings.py
+++ main_settings/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_backup'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
